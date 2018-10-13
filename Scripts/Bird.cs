4e469b8f26d879dc77eb2fb6fb1bf3d20c4f4a53
hjared7 <hjared7@vt.edu>
2018-10-13 12:37:18 -0400

    Birds

--- Scripts/Bird.cs
+++ Scripts/Bird.cs
@@ -2,24 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour {
+public class Bird : Entity_Script {
+
+    public float speed;
+    public float pickUpRange;
+    private bool holding;
 
     // Use this for initialization
-    void Start () {
-        timer = 5.0f;	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Intentionally blank
-	}
+    void Start() {
+        holding = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (possessed)
+        {
+            countDown();
+            interact();
+        }
+    }
 
     void interact()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.A))
         {
-            float hMove = Input.GetAxis("Horizontal");
-            velocity.x = hMove * maxSpeed;
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            if (holding)
+            {
+                player.transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            if (holding)
+            {
+                player.transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            if (holding)
+            {
+                player.transform.position += Vector3.up * speed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+            if (holding)
+            {
+                player.transform.position += Vector3.down * speed * Time.deltaTime;
+            }
+        }
+
+        float distanceToPlayer = Vector2.Distance(player.transform.position, gameObject.transform.position);
+        if (distanceToPlayer < pickUpRange)
+        {
+            holding = true;
+        }
+        if (timer <= 0 || this != GameObject.FindGameObjectWithTag("Possessed"))
+        {
+            holding = false;
         }
     }
 }
